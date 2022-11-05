ef8404c11432738fc158e211c425cfbb2dde3ba9
hyojung <rmr02090@gmail.com>
2022-11-05 17:14:22 +0900

    feat: 제품 목록 렌더링 #45-1

--- src/views/admin-product/admin-product.js
+++ src/views/admin-product/admin-product.js
@@ -1,22 +1,53 @@
 const tbody = document.querySelector('.table > tbody');
 let number = 0;
 let category = '';
-// let topCategoryTitle;
-let title = '';
+let productName = '';
+let title = ''; // 제조사
 let price = 0;
 let stock = 0;
 let createdAt = '';
 
-function displayList(number, category, price, createdAt) {
+function productListTemplate(
+  number,
+  productName,
+  category,
+  title,
+  price,
+  createdAt,
+) {
   return `
   <tr>
-    <td><input type="checkbox" /></td>
     <td>${number}</td>
     <td>${category}</td>
-    <td><a href="#">${price}</a></td>
+    <td><a href="/${'tempUrl'}"><span>[${title}]<span> ${productName}</a></td>
     <td>${price}</td>
     <td>${stock}</td>
     <td>${createdAt}</td>
     <td><button>수정</button></td>
   </tr>`;
 }
+
+// TODO 리팩토링, 비동기 공부
+// 제품 목록 렌더링
+fetch('./add.json')
+  .then((response) => response.json())
+  .then((list) => {
+    list.forEach((element) => {
+      number = element.index;
+      category = element.category;
+      productName = element.productName;
+      title = element.title;
+      price = element.price;
+      stock = element.stock;
+      createdAt = element.createdAt;
+
+      tbody.innerHTML += productListTemplate(
+        number,
+        productName,
+        category,
+        title,
+        price,
+        createdAt,
+      );
+    });
+  });

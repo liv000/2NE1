9d87145d4119c54a52a064179af48c1d372314e3
seungha <frazzle124@gmail.com>
2022-11-05 21:50:48 +0900

    feat:상품 상세조회

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -21,6 +21,14 @@ class ProductService {
       await this.productModel.updateStock(products[i]);
     }
   }
+  async getProductDetail(name, id) {
+    const productDetail = await this.productModel.findProductDetail(name);
+    console.log;
+    if (productDetail.id !== id) {
+      throw new Error('상품명과 상품아이디 다름');
+    }
+    return productDetail;
+  }
 }
 
 const productService = new ProductService(productModel);

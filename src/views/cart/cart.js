fe560ad8bab04d9b242953da8f0d0ef7765f1885
bm4656 <newjin46@gmail.com>
2022-11-06 02:19:35 +0900

    feat: 결제정보카드 더미데이터 테스트

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,11 +1,17 @@
 // cart.js
-import { data } from '../../db/dummy.js';
-
+import { addCommas, convertToNumber } from '../useful-functions.js';
+import { data } from './dummy.js';
 console.log('카트리스트');
 // 요소 모음
 const productList = document.querySelector('#products');
-// 데이터를 받아 요소를 만든 후, html에 삽입
+const productCount = document.querySelector('#product-count');
+const productTotal = document.querySelector('#product-total');
+const totalPrice = document.querySelector('#total-price');
+const deliveryPrice = document.querySelector('#delivery-price');
+
 insertProductElement();
+drawOrderCard();
+// 데이터를 받아 요소를 만든 후, html에 삽입
 function insertProductElement() {
   console.log(data);
   console.log('장바구니리스트 test');
@@ -27,11 +33,20 @@ function insertProductElement() {
             <p id="productCode">${code}</p>
           </div>
           <div class="price">
-            <h5 id="productPrice">${price}원</h5>
+            <h5 id="productPrice">${addCommas(price)}원</h5>
           </div>
         </div>
       </div>
     `,
     );
   });
+}
+
+//결제정보 카드에 상품 수와 가격 삽입
+async function drawOrderCard() {
+  let price = 20000;
+  productCount.innerHTML = `${data.total}개`;
+  productTotal.innerHTML = `${addCommas(price)}원`;
+  deliveryPrice.innerHTML = `3,000원`;
+  totalPrice.innerHTML = `${addCommas(price + 3000)}원`;
 }

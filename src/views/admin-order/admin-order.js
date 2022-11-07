30fd03b3c2322b84cbb0547d586b8303f16c654c
hyeji <12hyeji24@gmail.com>
2022-11-07 15:18:08 +0900

    admin order 수정

--- src/views/admin-order/admin-order.js
+++ src/views/admin-order/admin-order.js
@@ -1,116 +1,98 @@
-import { addCommas, checkAdmin, createNavbar } from "../../useful-functions.js";
-import * as Api from "../../api.js";
+// dummy에서 지정한 데이터 불러오기 가능
+// 주문 삭제 수정 필요
+import * as Api from "/api.js";
+import { data } from '/dummy.js';
 
-// 요소(element), input 혹은 상수
-const ordersContainer = document.querySelector("#ordersContainer");
+const admin_orderlist_table = document.querySelector("#admin-orderlist-table");
 const modal = document.querySelector("#modal");
 const modalBackground = document.querySelector("#modalBackground");
 const modalCloseButton = document.querySelector("#modalCloseButton");
 const deleteCompleteButton = document.querySelector("#deleteCompleteButton");
 const deleteCancelButton = document.querySelector("#deleteCancelButton");
 
-checkAdmin();
-addAllElements();
-addAllEvents();
+// fetch('http://localhost:3000/users', {method: 'GET', headers:{'Content-Type':'application/json'},})
+// .then(res=>res.json())
+// .then(data=> {
+//     data.forEach((item)=>{
+//         let date = item.createdAt.slice(0, 10);
+//     })
+// }
+//     )
+// .catch(error=>console.error('Error:',error));
+console.log(data.items[0].userId);
+console.log(data.items[0].title);
 
-// 요소 삽입 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllElements() {
-    createNavbar();
-    insertOrders();
-}
-
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
+drawOrderList()
+drawAllEvents()
+function drawAllEvents() {
     modalBackground.addEventListener("click", closeModal);
     modalCloseButton.addEventListener("click", closeModal);
-    document.addEventListener("keydown", keyDownCloseModal);
+
     deleteCompleteButton.addEventListener("click", deleteOrderData);
     deleteCancelButton.addEventListener("click", cancelDelete);
 }
-
-// 페이지 로드 시 실행, 삭제할 주문 id를 전역변수로 관리함
 let orderIdToDelete;
-async function insertOrders() {
-    const orders = await Api.get("/api/orderlist/all");
+async function drawOrderList(){
+    data.items.map((item) => {
 
-    for (const order of orders) {
-        const { _id, totalPrice, createdAt, summaryTitle, status } = order;
-        const date = createdAt.split("T")[0];
-    
-        ordersContainer.insertAdjacentHTML(
+        const { _id, userId, title, totalAmount, currStatus } = item;
+        // const date = createdAt.slice(0, 10);
+        admin_orderlist_table.insertAdjacentHTML(
             "beforeend",
             `
-                <div class="columns orders-item" id="order-${_id}">
-                    <div class="column is-2">${date}</div>
-                    <div class="column is-4 order-summary">${summaryTitle}</div>
-                    <div class="column is-2">${addCommas(totalPrice)}</div>
-                    <div class="column is-2">
+            <tbody>
+                <tr id="${_id}">
+                    <td>${userId}</td>
+                    <td>${title}</td>
+                    <td>${totalAmount}</td>
+                    <td>
                         <div class="select" >
-                            <select id="statusSelectBox-${_id}">
-                                <option 
-                                class="has-background-danger-light has-text-danger"
-                                ${status === "상품 준비중" ? "selected" : ""} 
-                                value="상품 준비중">
-                                    상품 준비중
+                            <select class="selectBtn${_id}">
+                                <option ${currStatus === "상품 준비중" ? "selected" : ""} 
+                                    value="상품 준비중">
+                                        상품 준비중
                                 </option>
-                                <option 
-                                class="has-background-primary-light has-text-primary"
-                                ${status === "상품 배송중" ? "selected" : ""} 
-                                value="상품 배송중">
-                                    상품 배송중
+                                <option ${currStatus === "상품 배송중" ? "selected" : ""} 
+                                    value="상품 배송중">
+                                        상품 배송중
                                 </option>
-                                <option 
-                                class="has-background-grey-light"
-                                ${status === "배송완료" ? "selected" : ""} 
-                                value="배송완료">
-                                    배송완료
+                                <option ${currStatus === "배송완료" ? "selected" : ""} 
+                                    value="배송완료">
+                                        배송완료
+                                </option>
+                                <option ${currStatus === "주문취소" ? "selected" : ""} 
+                                    value="주문취소">
+                                        주문취소
                                 </option>
                             </select>
                         </div>
-                    </div>
-                    <div class="column is-2">
-                        <button class="button" id="deleteButton-${_id}" >주문 취소</button>
-                    </div>
-                </div>
+                    </td>
+                    <td>
+                        <button class="button" id="deleteBtn${_id}" >주문 삭제</button>
+                    </td>
+                </tr>
+            </tbody>
             `
         );
-
-    // 요소 선택
-        const statusSelectBox = document.querySelector(`#statusSelectBox-${_id}`);
-        const deleteButton = document.querySelector(`#deleteButton-${_id}`);
-
-        // 상태관리 박스에, 선택되어 있는 옵션의 배경색 반영
-        const index = statusSelectBox.selectedIndex;
-        statusSelectBox.className = statusSelectBox[index].className;
-
-        // 이벤트 - 상태관리 박스 수정 시 바로 db 반영
-        statusSelectBox.addEventListener("change", async () => {
-            const newStatus = statusSelectBox.value;
+        const selectBtnBox = document.querySelector(`.selectBtn${_id}`);
+        const deleteBtn = document.querySelector(`#deleteBtn${_id}`);
+        const index = selectBtnBox.selectedIndex;
+        selectBtnBox.className = selectBtnBox[index].className;
+        selectBtnBox.addEventListener("change", async () => {
+            const newStatus = selectBtnBox.value;
             const data = { status: newStatus };
-
-        // 선택한 옵션의 배경색 반영
-            const index = statusSelectBox.selectedIndex;
-            statusSelectBox.className = statusSelectBox[index].className;
-
-        // api 요청
+            const index = selectBtnBox.selectedIndex;
+            selectBtnBox.className = selectBtnBox[index].className;
             await Api.patch("/api/orders", _id, data);
-        });
+    });
+    deleteBtn.addEventListener("click", () => {
+        orderIdToDelete = _id;
+        openModal();
+    });
+});
 
-    // 이벤트 - 삭제버튼 클릭 시 Modal 창 띄우고, 동시에, 전역변수에 해당 주문의 id 할당
-        deleteButton.addEventListener("click", () => {
-            orderIdToDelete = _id;
-            openModal();
-        });
-    }
-
-  // 총 요약 값 삽입
-    ordersCount.innerText = addCommas(summary.ordersCount);
-    prepareCount.innerText = addCommas(summary.prepareCount);
-    deliveryCount.innerText = addCommas(summary.deliveryCount);
-    completeCount.innerText = addCommas(summary.completeCount);
 }
 
-// db에서 주문정보 삭제
 async function deleteOrderData(e) {
     e.preventDefault();
 
@@ -129,30 +111,16 @@ async function deleteOrderData(e) {
 
         closeModal();
     } catch (err) {
-        alert(`주문정보 삭제 과정에서 오류가 발생하였습니다: ${err}`);
+        alert(`오류가 발생하였습니다: ${err}`);
     }
 }
-
-// Modal 창에서 아니오 클릭할 시, 전역 변수를 다시 초기화함.
 function cancelDelete() {
     orderIdToDelete = "";
     closeModal();
 }
-
-// Modal 창 열기
 function openModal() {
     modal.classList.add("is-active");
 }
-
-// Modal 창 닫기
 function closeModal() {
     modal.classList.remove("is-active");
-}
-
-// 키보드로 Modal 창 닫기
-function keyDownCloseModal(e) {
-  // Esc 키
-    if (e.keyCode === 27) {
-        closeModal();
-    }
 }

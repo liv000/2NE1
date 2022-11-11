6351c3af4ab041c46d9e1d911df7de096a48788d
seungha <frazzle124@gmail.com>
2022-11-11 19:48:35 +0900

    주문 상세정보 링크 수정

--- src/views/useful-functions.js
+++ src/views/useful-functions.js
@@ -41,3 +41,20 @@ export const getUrlParams = () => {
 
   return result;
 };
+
+export const formatPhoneNumber = (input) => {
+  const cleanInput = input.replaceAll(/[^0-9]/g, '');
+  let result = '';
+  const length = cleanInput.length;
+  if (length === 8) {
+    result = cleanInput.replace(/(\d{4})(\d{4})/, '$1-$2');
+  } else if (cleanInput.startsWith('02') && (length === 9 || length === 10)) {
+    result = cleanInput.replace(/(\d{2})(\d{3,4})(\d{4})/, '$1-$2-$3');
+  } else if (!cleanInput.startsWith('02') && (length === 10 || length === 11)) {
+    result = cleanInput.replace(/(\d{3})(\d{3,4})(\d{4})/, '$1-$2-$3');
+  } else {
+    result = undefined;
+  }
+  console.log(`${input} -> ${result}`);
+  return result;
+};
